44deeed4fa9b9ce7258246ec11a4c7f67210bf1c
Matan Banner <matan.banner@gmail.com>
2020-02-26 12:13:57 +0200

    Save the entire mongodb path in the env variable

--- src/db/monsooge.js
+++ src/db/monsooge.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`${process.env.MONGODB_USL}/task-manager-api`, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true
 })
