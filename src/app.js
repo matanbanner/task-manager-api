9a5891f5d7ae04d99176c4417977c89e436043d5
Matan Banner <matan.banner@gmail.com>
2020-03-08 16:57:02 +0200

    clean up

--- src/app.js
+++ src/app.js
@@ -4,11 +4,7 @@ require('./db/monsooge')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
-const User = require('../src/models/user')
-const Task = require('../src/models/task')
-
 const app = express()
-const port = process.env.PORT
 
 // app.use((req, res, next) => {
 //     res.status(503).send('The site under maintenance!')
